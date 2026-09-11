5edda3bcc1bf905b37a70b384106cf02e851d3e5
Jean-Pascal Steinmetz <steinmetzjeanpascal@gmail.com>
2026-09-11 17:29:24 +0200

    added and styled html header and created array with objects

---
+++ script.js
@@ -0,0 +1,28 @@
+const items = [
+  {
+    name: "Veggie mushroom black burger",
+    desc: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+    price: "16,90€",
+    img: `<img src="./assets/img/veggie-mushroom.png">`
+  },
+  {
+    name: "All meat burger",
+    desc: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",
+    price: "15,90€",
+    img: `<img src="./assets/img/all-meat.png">`
+  },
+  {
+    name: "Beef red burger",
+    desc: "Beef, Cheese, Tomatoes, Lettuce, Onion",
+    price: "14,90€",
+    img: `<img src="./assets/img/beef-red.png">`
+  },
+  {
+    name: "Big chicken burger",
+    desc: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+    price:"15,90€",
+    img:`<img src="./assets/img/big-chicken.png">`
+  }
+
+];
+
