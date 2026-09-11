8d8365801e718da263879706d27f3d4ae3e81cfb
Jean-Pascal Steinmetz <steinmetzjeanpascal@gmail.com>
2026-09-12 00:28:22 +0200

    added first section

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 const items = [
   {
     name: "Veggie mushroom black burger",
-    desc: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+    include: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
     price: "16,90€",
-    img: `<img src="./assets/img/veggie-mushroom.png">`
+    img: "assets/img/veggie-mushroom.png",
   },
   {
     name: "All meat burger",
-    desc: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",
+    include: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",
     price: "15,90€",
-    img: `<img src="./assets/img/all-meat.png">`
+    img: "assets/img/all-meat.png",
   },
   {
     name: "Beef red burger",
-    desc: "Beef, Cheese, Tomatoes, Lettuce, Onion",
+    include: "Beef, Cheese, Tomatoes, Lettuce, Onion",
     price: "14,90€",
-    img: `<img src="./assets/img/beef-red.png">`
+    img: "assets/img/beef-red.png",
   },
   {
     name: "Big chicken burger",
-    desc: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
-    price:"15,90€",
-    img:`<img src="./assets/img/big-chicken.png">`
-  }
-
+    include: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+    price: "15,90€",
+    img: "assets/img/big-chicken.png",
+  },
 ];
 
+function renderItems() {
+  for (let i = 0; i < items.length; i++) {
+    document.getElementById(`itemList`).innerHTML += `
+    
+<div class="single-dish">
+  <h3>${items[i].name}</h3>
+  <img src="${items[i].img}" alt="${items[i].name}">
+  <p class="preis">${items[i].price}</p>
+  <p class="include">${items[i].include}</p>
+  <button class="add-to-basket" onclick="addToBasket(${i})">Add to basket</button>
+</div>`
+  }
+}
+renderItems();
